11c862c016da78bd25096bace817335ca2c8b4e8
AndreFel13 <afcamargo17@gmail.com>
2022-01-15 11:44:16 -0300

    Configuração inicial do Projeto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PainelComponent } from './pages/painel/painel.component';
+import { InicialComponent } from './pages/inicial/inicial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent,
+    HomeComponent,
+    LoginComponent,
+    PainelComponent,
+    InicialComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/pages/inicial/inicial.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InicialRoutingModule } from './inicial-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InicialRoutingModule
+  ]
+})
+export class InicialModule { }
